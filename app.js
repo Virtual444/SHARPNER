25067e8e3bf06af11d8c9512e0257594560e7c45
asitpal211 <asitpal211@gmail.com>
2023-08-18 17:40:51 +0530

    Serving HTML , CSS and JS with contact us page

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+// Serve static files from the 'views' directory
+
+// Route for the contact form
+app.get('/contactus', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contactus.html'));
 });
 
-app.listen(3000); 
+app.get('/success', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'views', 'success.html'));
+});
+// Route for the success page
+app.post('/success', (req, res) => {
+    // Handle form submission here (store data, etc.)
+    // For this example, we're just redirecting to the success page
+    res.redirect('/success');
+});
+
+// Route for handling "Page not found" errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
