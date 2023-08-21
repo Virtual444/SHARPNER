f774f6ce1acda6bfbf907c40bcc43fe7c0149e72
asitpal211 <asitpal211@gmail.com>
2023-08-21 20:18:01 +0530

    all functionality with sequelize

--- online shop/app.js
+++ online shop/app.js
@@ -7,7 +7,7 @@ const errorController = require('./controllers/error');
 
 
 
-
+const  sequelize = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -26,4 +26,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+sequelize
+.sync()
+.then( result => {
+    app.listen(3000);
+})
+.catch( err => {
+    console.log(err);
+});
